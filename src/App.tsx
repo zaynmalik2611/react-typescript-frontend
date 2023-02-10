a5cbecf7f84715b35c958d61f40a49e706b6e7b0
zaynmalik2611 <zabideen.bscs17seecs@seecs.edu.pk>
2023-02-11 03:09:23 +0500

    add: setup react-router in the project

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      Free the mind
       <Button>hello</Button>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
