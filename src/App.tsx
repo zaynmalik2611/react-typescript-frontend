882c9ac3b2badf8cbd3f118890c854aafb251bc2
zaynmalik2611 <zabideen.bscs17seecs@seecs.edu.pk>
2023-02-11 02:39:34 +0500

    add: add navbar comp and prettierrc file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+import "./App.css";
+import Navbar from "./components/header/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       Free the mind
-      <Button >hello</Button>
+      <Button>hello</Button>
     </div>
   );
 }
---
+++ src/components/header/Navbar.tsx
@@ -0,0 +1,22 @@
+import Nav from "react-bootstrap/Nav";
+import { NavItem, NavLink } from "react-bootstrap";
+// import { Item, Link } from 'react-bootstrap/Nav'
+
+function Navbar() {
+  //   const hello = "hello";
+  return (
+    <Nav justify variant="tabs" defaultActiveKey="/home">
+      <NavItem>
+        <NavLink eventKey="active">Active</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink eventKey="option-1">Option 2</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink eventKey="third">Third</NavLink>
+      </NavItem>
+    </Nav>
+  );
+}
+
+export default Navbar;
